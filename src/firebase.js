e40b4e5cd7dcad0acdefd03945d0fd8639021578
nemosakukiri <nemosakukiri@gmail.com>
2026-03-22 12:42:16 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5K73evEK33c3VIOlIEkOz2c1IRAiltWM",
+  authDomain: "fusakui-db.firebaseapp.com",
+  projectId: "fusakui-db",
+  storageBucket: "fusakui-db.firebasestorage.app",
+  messagingSenderId: "520726255572",
+  appId: "1:520726255572:web:1d3151dfb01c36ccdf3211"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
